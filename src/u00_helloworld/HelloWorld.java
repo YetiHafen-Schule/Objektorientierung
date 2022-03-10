0b0fb46d3b6a5d5bb75f91c82d5b737651ff13d7
YetiHafen <yetihafen734@gmail.com>
2022-03-10 10:13:37 +0100

    u00_helloworld

---
+++ src/u00_helloworld/HelloWorld.java
@@ -0,0 +1,10 @@
+package u00_helloworld;
+
+public class HelloWorld {
+
+    String text = "Hello, World!";
+
+    public void schreiben() {
+        System.out.println(text);
+    }
+}
---
+++ src/u00_helloworld/HelloWorldProgramm.java
@@ -0,0 +1,9 @@
+package u00_helloworld;
+
+public class HelloWorldProgramm {
+
+    public static void main(String[] args) {
+        HelloWorld hw = new HelloWorld();
+        hw.schreiben();
+    }
+}
